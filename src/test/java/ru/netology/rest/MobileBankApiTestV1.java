db360b4130294d8a8ec01ba3abb0e017f9bbfd5f
pachimar1 <mihail.lebedev88@gmail.com>
2023-06-16 14:17:49 +0600

    correct json schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,22 +1,31 @@
 package ru.netology.rest;
 
+import io.restassured.specification.Argument;
 import org.junit.jupiter.api.Test;
+import io.restassured.http.ContentType;
 
+import java.util.List;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.given;
+
 class
 
 MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                ;
     }
 }
